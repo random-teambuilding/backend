0ec51401faf979faf4cd6bdc2a804d3ceb5371c4
TitanV <a01098930999@gmail.com>
2020-10-30 21:16:10 +0900

    update - 폴더구조 변화
    koa.js에 맞는 폴더구조로 만들었습니다

--- app.js
+++
@@ -1,8 +0,0 @@
-const models = require("./models/index.js");
-
-models.sequelize.sync().then( () => {
-    console.log("DB 연결 성공");
-}).catch(err => {
-    console.log("연결 실패");
-    console.log(err);
-})
